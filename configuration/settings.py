9014e2689668b1a622a46b5e1b42a576e5b6023f
Qurban Mohebi <q.mohebi@gmail.com>
2026-06-22 10:27:05 +0100

    updated settings for production

--- configuration/settings.py
+++ configuration/settings.py
@@ -338,6 +338,4 @@ if not DEBUG:
 
     SESSION_COOKIE_AGE = 15 * 60
     SESSION_SAVE_EVERY_REQUEST = True
-
-# optional logout when browser closes
-# SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+    SESSION_EXPIRE_AT_BROWSER_CLOSE = True
